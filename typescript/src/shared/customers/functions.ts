ac540563b1eda546ba3e61ff287cf1a64ef58cee
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2025-04-07 14:50:36 +0530

    fix: list customers function should return customer objects instead of ids

--- typescript/src/shared/customers/functions.ts
+++ typescript/src/shared/customers/functions.ts
@@ -31,7 +31,7 @@ export const listCustomers = async (
       context.account ? {stripeAccount: context.account} : undefined
     );
 
-    return customers.data.map((customer) => ({id: customer.id}));
+    return customers.data;
   } catch (error) {
     return 'Failed to list customers';
   }

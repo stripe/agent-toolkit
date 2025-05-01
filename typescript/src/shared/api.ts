9317902a4669c92b76af4a158fa93fbc951c1301
Steve Kaliski <stevekaliski@stripe.com>
2025-05-01 10:19:45 -0400

    bump version

--- typescript/src/shared/api.ts
+++ typescript/src/shared/api.ts
@@ -20,7 +20,7 @@ class StripeAPI {
           context?.mode === 'modelcontextprotocol'
             ? MCP_HEADER
             : TOOLKIT_HEADER,
-        version: '0.7.0',
+        version: '0.7.1',
         url: 'https://github.com/stripe/agent-toolkit',
       },
     });
